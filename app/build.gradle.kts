a510044a529f6ed64674a7c33170abcf827a590e
Stas Steblyanskiy <ssteblyanskiy@bk.ru>
2026-05-02 12:36:33 +0300

    pre-build changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,38 @@
 plugins {
     id("java")
+    id("checkstyle")
+    jacoco
+    application
+    checkstyle
+    id("org.sonarqube") version "7.0.1.6134"
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+checkstyle {
+    toolVersion = "10.12.4"
+    configFile = file("config/checkstyle/checkstyle.xml")
+    isIgnoreFailures = false
+    maxWarnings = 0
+}
+
+jacoco {
+    toolVersion = "0.8.14"
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+    }
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
 
 repositories {
     mavenCentral()
@@ -15,6 +44,23 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.test {
+sonar {
+    properties {
+        property("sonar.projectKey", "SaintCap_java-project-72")
+        property("sonar.organization", "saintcap")
+    }
+}
+
+tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+tasks {
+    checkstyleMain {
+        dependsOn(compileJava)
+    }
+
+    checkstyleTest {
+        dependsOn(compileTestJava)
+    }
 }
