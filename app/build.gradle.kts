010d08e55a3e4d62e0e27bdde7a5fe1f053dcb1a
Stas Steblyanskiy <ssteblyanskiy@bk.ru>
2026-06-15 17:46:24 +0300

    javalin up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,9 +43,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("io.javalin:javalin-testtools:6.1.3")
+    testImplementation("io.javalin:javalin-testtools:6.6.0")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
-    implementation("io.javalin:javalin:6.1.3")
+    implementation("io.javalin:javalin:6.6.0")
     implementation("org.slf4j:slf4j-simple:2.0.12")
     implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("com.h2database:h2:2.2.224")
